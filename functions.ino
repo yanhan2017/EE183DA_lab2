85a860adcd87426097e4ae54b52cc6d4fda4e53f
yanhan2017 <yanhan2014@icloud.com>
2017-02-07 12:51:09 -0800

    Add files via upload

--- functions.ino
+++ functions.ino
@@ -1,68 +1,50 @@
 void play(int pos)
 {
+  sr.writeMicroseconds(1400);
   int diff = pos - curr_pos;
   
   if(diff >= 0)
   {
-    sr.write(90 + 90/npos*diff);
-    delay(period);
+    sr.write(90 + 90/(npos+2)*diff);
+    delay(period + delays*diff*sqrt(diff));
+    sr.writeMicroseconds(1400);
   }
   else
   {
-    sr.write(85 + 85/npos*diff);
-    delay(period);
+    sr.write(85 + 85/(npos)*diff);
+    delay(period + delays*(-diff)*sqrt(-diff));
+    sr.writeMicroseconds(1400);
   } 
-   sr.writeMicroseconds(1400);
+   
    //s.write(45);
    //delay(100);
    s.write(0);
-   delay(100);
-   s.write(45);
-   delay(100);
-   curr_pos = pos;
-}
-
-void doublestroke(int pos)
-{
-  int diff = pos - curr_pos;
-  sr.write(90 + 90/npos*diff);
-  if(diff >= 0)
-  {
-    delay(period);
-  }
-  else
-  {
-    delay(period*ratio);
-  }
    sr.writeMicroseconds(1400);
-   s.write(45);
    delay(100);
-   s.write(0);
-   delay(150);
-   delay(period/2);
    s.write(45);
-   delay(100);
-   s.write(0);
+   sr.writeMicroseconds(1400);
    delay(100);
    curr_pos = pos;
+   sr.writeMicroseconds(1400);
 }
 
 void endsong()
 {
+  sr.writeMicroseconds(1400);
   int diff = 0 - curr_pos;
   
   if(diff >= 0)
   {
-    sr.write(90 + 90/npos*diff);
-    delay(period);
+    sr.write(90 + 90/(npos+2)*diff);
+    delay(period + delays*diff*sqrt(diff));
   }
   else
   {
-    sr.write(85 + 85/npos*diff);
-    delay(period);
+    sr.write(85 + 85/(npos)*(diff));
+    delay(period + delays*(-diff)*sqrt(-diff));
   }
-  sr.writeMicroseconds(1400);
   curr_pos = 0;
+  sr.writeMicroseconds(1400);
 }
 
 void Twinkle()
@@ -75,6 +57,7 @@ void Twinkle()
   play(6);
   play(5);
   delay(period);
+  
   play(4);
   play(4);
   play(3);
@@ -82,7 +65,41 @@ void Twinkle()
   play(2);
   play(2);
   play(1);
+  delay(period);
   
-  endsong;
+  /*play(5);
+  play(5);
+  play(4);
+  play(4);
+  play(3);
+  play(3);
+  play(2);
+  delay(period);
+  
+  play(5);
+  play(5);
+  play(4);
+  play(4);
+  play(3);
+  play(3);
+  play(2);
+  delay(period);
+  play(1);
+  play(1);
+  play(5);
+  play(5);
+  play(6);
+  play(6);
+  play(5);
+  delay(period);
+  
+  play(4);
+  play(4);
+  play(3);
+  play(3);
+  play(2);
+  play(2);
+  play(1);*/
+  endsong();
 }
 
--- run_motor.ino
+++ run_motor.ino
@@ -6,11 +6,10 @@ WiFiServer server(80);
 Servo s;
 Servo sr;
 
-int ratio = 1.5;
-int angle = 0;
-int npos = 8; //total number of positions
-int period = 250;
+int npos = 6; //total number of positions
+int period = 200;
 int curr_pos;
+int delays = 40;
 //unsigned long Timer = mills();
 
 void setup()
@@ -21,6 +20,7 @@ void setup()
   s.attach(D3);
   sr.attach(D4);
   curr_pos = 0; //start at before the leftmost position
+  sr.writeMicroseconds(1400);
 }
 
 void loop()
@@ -56,11 +56,7 @@ void loop()
    {
      play(6); 
    }
-   else if (request.indexOf("/play7") != -1)
-   {
-     play(7); 
-   }
-   else if (request.indexOf("/Twinkle Twinkle Little Star") != -1)
+   else if (request.indexOf("/Twinkle") != -1)
    {
      Twinkle(); 
    }
@@ -79,7 +75,6 @@ void loop()
    s += "<br><input type=\"button\" name=\"b1\" value=\"play4\" onclick=\"location.href='/play4'\">";
    s += "<br><input type=\"button\" name=\"b1\" value=\"play5\" onclick=\"location.href='/play5'\">";
    s += "<br><input type=\"button\" name=\"b1\" value=\"play6\" onclick=\"location.href='/play6'\">";
-   s += "<br><input type=\"button\" name=\"b1\" value=\"play7\" onclick=\"location.href='/play7'\">";
    s += "<br><br><br>";
    s += "<br><input type=\"button\" name=\"b1\" value=\"Twinkle Twinkle Little Star\" onclick=\"location.href='/Twinkle'\">";
    s += "<br><input type=\"button\" name=\"b1\" value=\"endsong\" onclick=\"location.href='/endsong'\">";
@@ -88,50 +83,5 @@ void loop()
    client.flush(); //clear previous info in the stream
    client.print(s); // Send the response to the client
    delay(1);
-  //doublestroke(1);
-  //doublestroke(2);
-  /*play(1);
-  play(3);
-  play(2);
-  play(1);
-  
-  delay(3000);*/
-  /*doublestroke(1);
-  play(2);
-  play(1);
-  play(4);
-  play(3);
-  doublestroke(1);
-  play(2);
-  play(1);
-  play(5);
-  play(4);
-  doublestroke(1);
-  play(8);
-  play(6);
-  play(4);
-  play(3);
-  play(2);
-  doublestroke(7);
-  play(6);
-  play(4);
-  play(5);
-  play(4);
-  endsong();*/
-  /*sr.write(0);
-  delay(500);
-  sr.write(90);
-  s.write(90);
-  delay(200);
-  s.write(0);
-  delay(200);
-
-  sr.write(180);
-  delay(500);
-  sr.write(90);
-  s.write(90);
-  delay(200);
-  s.write(0);
-  delay(200);*/
 }
 
